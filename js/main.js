de62c490d677e207a8b0f33e7e6082167f36f87a
Taras <baraniuktaras098@gmail.com>
2022-12-27 20:57:23 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const burgerMenu = document.getElementById('burgerMenu')
+const headerMenu = document.getElementById('headerMenu')
+const bodyOverlay = document.querySelector('body')
+
+const headerDropdown = document.querySelectorAll('#headerDropdown')
+
+burgerMenu.addEventListener('click', (e) => {
+    if (e.target.src) {
+        e.target.src = e.target.src.includes("/images/icon-close-menu.svg") ? "/images/icon-menu.svg" : "/images/icon-close-menu.svg"
+        headerMenu.classList.toggle('visible')
+        bodyOverlay.classList.toggle('overlay')
+    }
+})
+
+headerDropdown.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        console.log(e.target)
+        dropDownId = e.target.getAttribute('dropdownTo')
+        e.target.classList.toggle('active')
+        document.getElementById(dropDownId).classList.toggle('visible')
+})
+})
+
